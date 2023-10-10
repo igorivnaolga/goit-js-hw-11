a3e9ded933428284ebe309ced6aebaba9ea112fd
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-10-10 20:36:32 +1300

    add go-top button

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ let options = {
 const observer = new IntersectionObserver(onLoadMore, options);
 const scroll = new OnlyScrollbar(
   document.querySelector('.container', {
-    damping: 0.7,
+    damping: 0.9,
     eventContainer: window,
     mode: 'free',
   })
@@ -200,6 +200,30 @@ function renderMarkup(images) {
   });
 }
 
+const goTopBtn = document.querySelector('.go-top');
+
+window.addEventListener('scroll', trackScroll);
+
+goTopBtn.addEventListener('click', goTop);
+
+function trackScroll() {
+  const scrolled = window.pageYOffset;
+
+  const coords = document.documentElement.clientHeight;
+
+  if (scrolled > coords) {
+    goTopBtn.classList.add('go-top--show');
+  } else {
+    goTopBtn.classList.remove('go-top--show');
+  }
+}
+
+function goTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -25); //другий аргумент - це швидкість скролу
+    setTimeout(goTop, 0);
+  }
+}
 // function updateLoadMoreButton(totalHits) {
 //   const totalPages = Math.ceil(totalHits / perPage);
 //   console.log(totalPages);
